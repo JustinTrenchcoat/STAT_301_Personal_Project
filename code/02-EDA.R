8d92e70ace6d7f02dada7d86f2d82d009f68eea3
JustinTrenchcoat <justinchang2021@outlook.com>
2025-04-24 20:06:02 -0700

    done packageing and tested running package

--- code/02-EDA.R
+++ code/02-EDA.R
@@ -4,6 +4,7 @@ library(tibble)
 library(tidyverse)
 library(dplyr)
 library(readr)
+library(stat301Proj)
 
 "This script reads restaurant data from folder, runs some EDA on it.
 
@@ -17,16 +18,18 @@ opt <- docopt(doc)
 
 data_tips <- read_rds(opt$data_path)
 
-# convert character variables into factors
-data_tips$sex <- as.factor(data_tips$sex)
-data_tips$smoker <- as.factor(data_tips$smoker)
-data_tips$day <- as.factor(data_tips$day)
-data_tips$time <- as.factor(data_tips$time)
+summary_table <- summaryTable(data_tips)
 
-summary_table <- tibble(
-  variable = names(data_tips),
-  type = sapply(data_tips, function(x) class(x)[1]),
-  n_levels = sapply(data_tips, function(x) if(is.factor(x)) nlevels(x) else NA),
-  have_NA = sapply(data_tips, function(x) any(is.na(x)))
-)
+# # convert character variables into factors
+# data_tips$sex <- as.factor(data_tips$sex)
+# data_tips$smoker <- as.factor(data_tips$smoker)
+# data_tips$day <- as.factor(data_tips$day)
+# data_tips$time <- as.factor(data_tips$time)
+
+# summary_table <- tibble(
+#   variable = names(data_tips),
+#   type = sapply(data_tips, function(x) class(x)[1]),
+#   n_levels = sapply(data_tips, function(x) if(is.factor(x)) nlevels(x) else NA),
+#   have_NA = sapply(data_tips, function(x) any(is.na(x)))
+# )
 write_rds(summary_table, opt$output_path)
